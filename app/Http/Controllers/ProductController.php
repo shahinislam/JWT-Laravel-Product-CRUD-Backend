2dcbf253ff57c319da430be90fdfb9128b5bbd6f
Shahinur Islam <shahinislam8989@gmail.com>
2021-08-01 22:33:41 +0600

    first commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,172 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $products = auth()->user()->products()->get();
+
+        return response()->json($products, 200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $rules = [
+            'title' => 'required',
+            'description' => 'required',
+            'price' => 'required',
+            'image' => 'nullable|image:png,jpg,jpeg',
+        ];
+
+        $validator = Validator::make($request->all(), $rules);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+
+        if ($request->image) {
+            $imagePath = $request->image->store('product_images', 'public');
+            $imageArray = ['image' => $imagePath];
+        }
+
+        $product = auth()->user()->products()->create(array_merge(
+            $request->all(),
+            $imageArray ?? [],
+        ));
+
+        return response()->json($product, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Product $product)
+    {
+        if (is_null($product)) {
+            return response()->json(['message' => 'Record not found'], 404);
+        }
+
+        return response()->json($product, 200);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Product $product)
+    {
+        if ($product->user_id == auth()->user()->id) {
+            return response()->json($product, 200);
+        } else {
+            return response()->json(['message' => 'Record not found'], 404);
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Product $product)
+    {
+
+        if (is_null($product)) {
+            return response()->json(['message' => 'Record not found'], 404);
+        }
+
+        $rules = [
+            'title' => 'required',
+            'description' => 'required',
+            'price' => 'required',
+            'image' => 'nullable|image:png,jpg,jpeg',
+        ];
+
+        $validator = Validator::make($request->all(), $rules);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+
+        if ($request->image) {
+            if ($product->image) {
+                Storage::delete('public/' . $product->image);
+            }
+
+            $imagePath = $request->image->store('product_images', 'public');
+            $imageArray = ['image' => $imagePath];
+
+            $product = $product->update(array_merge(
+                $request->all(),
+                $imageArray ?? [],
+            ));
+
+        } else {
+
+            $product = $product->update([
+                'title' => $request->title,
+                'description' => $request->description,
+                'price' => $request->price,
+            ]);
+
+        }
+
+
+        return response()->json($product, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Product $product)
+    {
+        if (is_null($product)) {
+            return response()->json(['message' => 'Record not found'], 404);
+        }
+
+        if ($product->image) {
+            Storage::delete('public/' . $product->image);
+        }
+
+        $product->delete();
+
+        return response()->json(null, 204);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,47 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::group([
+
+    // 'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function () {
+
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::post('me', [AuthController::class, 'me']);
+
+});
+
+
+Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
+Route::post('/products', [ProductController::class, 'store'])->name('products.store');
+Route::get('/products/{product}/edit', [ProductController::class, 'edit'])->name('products.edit');
+Route::patch('/products/{product}', [ProductController::class, 'update'])->name('products.update');
+Route::get('/products/{product}', [ProductController::class, 'show'])->name('products.show');
+Route::delete('/products/{product}', [ProductController::class, 'destroy'])->name('products.destroy');
+
+// Route::resource('products', ProductController::class);
